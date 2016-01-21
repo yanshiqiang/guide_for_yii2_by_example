ff92827977c970bd43be249a7537723c90717299
yanshiqiang <yxyw@hotmail.com>
2016-01-21 20:49:45 +0800

    a menu layout apply

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -8,6 +8,7 @@ use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
+use yii\widgets\Menu;
 
 AppAsset::register($this);
 ?>
@@ -36,13 +37,39 @@ AppAsset::register($this);
    
     NavBar::end();
     ?>
-
-    <div class="container">
-        <?= Breadcrumbs::widget([
-            'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
-        ]) ?>
-        <?= $content ?>
-    </div>
+    
+	
+	
+	<div class="container">
+	   <?= Breadcrumbs::widget([
+		   'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
+	   ]) ?>
+	   <div class="col-sm-2">
+	      <?php
+			echo Menu::widget([
+						'items' => [
+								['label' => 'Home', 'url' => ['site/index']],
+								['label' => '性别', 'url' => ['site/gender']],
+								['label' => '年龄', 'url' => ['site/age']],
+								['label' => '省市', 'url' => ['site/area']],
+								['label' => '年级', 'url' => ['site/grade']],
+								['label' => '专业', 'url' => ['site/major']],
+								['label' => '校园网', 'url' => ['site/net']],
+								['label' => '校园无线网', 'url' => ['site/wlan']],
+								['label' => '年消费分布', 'url' => ['site/purchase']],
+								['label' => '生日分布', 'url' => ['site/birthday']],
+								['label' => '校友分布', 'url' => ['site/alumnus']],
+								['label' => '星座', 'url' => ['site/constellation']],
+							],
+						'activeCssClass'=>'activeclass',
+			]);
+		?>
+	   </div>
+	   <div class="col-sm-10">
+			<?= $content ?>
+	  </div>
+	</div>
+	
 </div>
 
 <footer class="footer">
