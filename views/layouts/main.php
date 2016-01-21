1a5fba1c11a05d9d05c210db7de8ffe47cbd50a8
yanshiqiang <yxyw@hotmail.com>
2016-01-21 10:32:37 +0800

    trim basic template
    only print the "hello world"

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -33,21 +33,7 @@ AppAsset::register($this);
             'class' => 'navbar-inverse navbar-fixed-top',
         ],
     ]);
-    echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ?
-                ['label' => 'Login', 'url' => ['/site/login']] :
-                [
-                    'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                    'url' => ['/site/logout'],
-                    'linkOptions' => ['data-method' => 'post']
-                ],
-        ],
-    ]);
+   
     NavBar::end();
     ?>
 
