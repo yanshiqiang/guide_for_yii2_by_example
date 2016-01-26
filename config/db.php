2c602d5b2a5abfc4a31e4b9f428abc0b6e13fa08
cxt521 <984156424@qq.com>
2016-01-26 16:18:48 +0800

    edit db.php

--- config/db.php
+++ config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=guide',
     'username' => 'root',
-    'password' => '1qaz2wsx',
+    'password' => '',
     'charset' => 'utf8',
 ];
